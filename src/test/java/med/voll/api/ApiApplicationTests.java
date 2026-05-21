6ad01bf082c56b7c9aa1b17ea3b828d27e2958cf
Isabel Melo <40863355+isamelo@users.noreply.github.com>
2026-05-21 15:54:14 -0300

    Add application context validation test

--- src/test/java/med/voll/api/ApiApplicationTests.java
+++ src/test/java/med/voll/api/ApiApplicationTests.java
@@ -2,12 +2,24 @@ package med.voll.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void applicationContextShouldLoadSuccessfully() {
+		assertThat(applicationContext).isNotNull();
+		assertThat(applicationContext.getApplicationName()).isNotEmpty();
 	}
 
 }
